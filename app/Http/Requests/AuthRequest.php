7809cf0174c4fcdc1a7646e9e8417fa6e4c2a6e1
VladislavPanda <pepartvlad7@gmail.com>
2024-09-04 23:07:24 +0300

    Created basic auth request

---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:6',
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Поле Email является обязательным',
+            'email.email' => 'Введите корректный email',
+            'email.exists' => 'Ошибка! Введённый email не существует',
+            'password.required' => 'Ввод пароля является обязательным',
+            'password.min' => 'Минимальное количество символов в пароле: 6'
+        ];
+    }
+}
