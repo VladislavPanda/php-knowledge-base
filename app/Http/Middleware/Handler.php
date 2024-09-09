b8bd5910a471c9ef41252f75b3e045ab78b2017a
VladislavPanda <pepartvlad7@gmail.com>
2024-09-10 00:04:40 +0300

    Created basic middlewares setup approach

---
+++ app/Http/Middleware/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Configuration\Middleware as BaseMiddleware;
+
+final class Handler
+{
+    /**
+     * @var array|class-string[]
+     */
+    protected array $aliases = [
+        'auth.internal' => AuthInternalMiddleware::class
+    ];
+
+    /**
+     * @param BaseMiddleware $baseMiddleware
+     * @return BaseMiddleware
+     */
+    public function __invoke(BaseMiddleware $baseMiddleware): BaseMiddleware
+    {
+        if ($this->aliases) {
+            $baseMiddleware->alias($this->aliases);
+        }
+
+        return $baseMiddleware;
+    }
+}
