219c5e65ba1b511db11fcdbfaf85d468a9d947f9
VladislavPanda <pepartvlad7@gmail.com>
2024-09-10 00:13:51 +0300

    Created auth internal middleware

---
+++ app/Http/Middleware/AuthInternalMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthInternalMiddleware
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return Auth::check() ? $next($request) : redirect()->route('welcome');
+    }
+}
