21cf0762da61f1ddcabaf121fce693c7e4499d52
VladislavPanda <pepartvlad7@gmail.com>
2024-09-10 00:16:08 +0300

    Added init routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', WelcomeController::class)->name('welcome');
-Route::post('/auth', AuthController::class)->name('auth');
+Route::post('/auth', [AuthController::class, 'login'])->name('auth');
+
+Route::middleware(['auth.internal', 'cache.prevent'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
