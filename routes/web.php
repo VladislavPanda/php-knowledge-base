8a170e26b7a5c2ed41adaa3e7a0a0d8fcf2b29ac
VladislavPanda <pepartvlad7@gmail.com>
2024-09-10 23:38:52 +0300

    Added route for chapter show and guest middleware for welcome route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ChapterController;
 
-Route::get('/', WelcomeController::class)->name('welcome');
+Route::get('/', WelcomeController::class)->name('welcome')->middleware('guest');
 Route::post('/auth', [AuthController::class, 'login'])->name('auth');
 
 Route::middleware(['auth.internal', 'cache.prevent'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/chapter/{id}', [ChapterController::class, 'index'])->name('chapter.show');
 });
