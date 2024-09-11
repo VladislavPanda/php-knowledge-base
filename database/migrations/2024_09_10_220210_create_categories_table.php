b1c2394307ad28c1deaeab0aecaf6c214ec0793e
VladislavPanda <pepartvlad7@gmail.com>
2024-09-11 23:37:24 +0300

    Removed unnecessary length definition

--- database/migrations/2024_09_10_220210_create_categories_table.php
+++ database/migrations/2024_09_10_220210_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('chapter_id');
-            $table->string('title', 255);
+            $table->string('title');
             $table->foreign('chapter_id')->references('id')->on('chapters')->onDelete('cascade');
 
             $table->timestamps();
