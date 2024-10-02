daaf7543459a1afcfa530ffeb006d60472742eb3
VladislavPanda <pepartvlad7@gmail.com>
2024-10-03 00:19:32 +0300

    Created basic Question model and migration

---
+++ app/Models/Question.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Question extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = 'questions';
+
+    /**
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function subCategory(): BelongsTo
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
+}
