844b241bcd389a1f74e91ca71812dbd94f9ef2a3
VladislavPanda <pepartvlad7@gmail.com>
2024-10-05 23:34:59 +0300

    Added link in dashboard title

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('dashboard') }}" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -45,7 +45,7 @@
                         </a>-->
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="{{ route('chapter.show', $chapter->getId()) }}" class="nav-link">
+                                <a href="{{ route('chapter.index', $chapter->getId()) }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>{{ $chapter->getTitle() }}</p>
                                 </a>
