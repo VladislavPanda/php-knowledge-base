3967f0e2b679de8107a9aedcf882ea186ce127f6
VladislavPanda <pepartvlad7@gmail.com>
2024-10-05 23:54:17 +0300

    Added User and Role Model relations

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
@@ -19,4 +20,12 @@ class Role extends Model
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
