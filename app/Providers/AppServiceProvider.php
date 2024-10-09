23752c91532f6a7089304e771dffe46283c4dc79
VladislavPanda <pepartvlad7@gmail.com>
2024-10-10 00:08:02 +0300

    Added basic roles functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use App\Contracts\ContentModelInterface;
+use App\Models\Chapter;
+use App\Policies\ContentPolicy;
 use App\Repositories\CategoryRepository;
 use App\Repositories\ChapterRepository;
 use App\Repositories\Contracts\CategoryRepositoryInterface;
 use App\Repositories\Contracts\ChapterRepositoryInterface;
+use App\Repositories\Contracts\RoleRepositoryInterface;
+use App\Repositories\RoleRepository;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(RoleRepositoryInterface::class, RoleRepository::class);
     }
 
     /**
@@ -23,6 +30,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(ContentModelInterface::class, ContentPolicy::class);
     }
 }
