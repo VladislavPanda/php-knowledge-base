6cc6430abb9bb5b3ab6d1b52e5ec1bffe8462a03
VladislavPanda <pepartvlad7@gmail.com>
2024-10-23 23:47:24 +0300

    Added sub category and question routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ChapterController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\QuestionController;
+use App\Http\Controllers\Admin\SubCategoryController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -15,4 +17,6 @@ Route::middleware(['auth.internal', 'cache.prevent'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/chapter/{id}', [ChapterController::class, 'index'])->name('chapter.index');
     Route::get('/category/{id}', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/sub-category/{id}', [SubCategoryController::class, 'index'])->name('subCategory.index');
+    Route::get('/question/{id}', [QuestionController::class, 'show'])->name('question.show');
 });
