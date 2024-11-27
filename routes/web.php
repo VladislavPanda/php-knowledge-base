b2c5a3293c236b195bcee4c4f3e823f29134c37a
VladislavPanda <pepartvlad7@gmail.com>
2024-11-28 00:42:59 +0300

    Added Google oAuth and User page routes

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,15 @@ use App\Http\Controllers\Admin\ChapterController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\QuestionController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class)->name('welcome')->middleware('guest')->middleware('cache.prevent');
 Route::post('/auth', [AuthController::class, 'login'])->name('auth');
+Route::get('auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('callback/google', [AuthController::class, 'handleGoogleCallback']);
 
 Route::middleware(['auth.internal', 'cache.prevent'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
@@ -19,4 +22,5 @@ Route::middleware(['auth.internal', 'cache.prevent'])->group(function () {
     Route::get('/category/{id}', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/sub-category/{id}', [SubCategoryController::class, 'index'])->name('subCategory.index');
     Route::get('/question/{id}', [QuestionController::class, 'show'])->name('question.show');
+    Route::get('/user/me/{id}', [UserController::class, 'show'])->name('user.show');
 });
